e9d28219a3942fc24386b0cf40c98b26a951b577
SquawkyToucan <ewbrowne743@gmail.com>
2017-09-09 16:28:32 -0700

    Finished GuestBook
    Moving on to HashMaps now.

--- src/IntroToArrayLists/GuestBook.java
+++ src/IntroToArrayLists/GuestBook.java
@@ -1,6 +1,15 @@
 package IntroToArrayLists;
 
-public class GuestBook {
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements MouseListener {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +18,62 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	ArrayList<String> names = new ArrayList<String>();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton addName = new JButton();
+	JButton viewNames = new JButton();
+	public static void main(String[] args) {
+		new GuestBook().run();
+	}
+	public GuestBook() {
+		//Create the GUI
+		addName.setText("Add a Name");
+		viewNames.setText("View Names");
+		addName.addMouseListener(this);
+		viewNames.addMouseListener(this);
+		panel.add(addName);
+		panel.add(viewNames);
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	public void run() {
+		names.add("Bob Banders");
+		names.add("Sandy Summers");
+		names.add("Greg Ganders");
+		names.add("Donny Doners");
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		Object buttonPressed = e.getSource();
+		if(buttonPressed == addName) {
+			String name = JOptionPane.showInputDialog("Please name the guest you wish to add.");
+			names.add(name);
+		}
+		if(buttonPressed == viewNames) {
+			JOptionPane.showMessageDialog(null, names.toString());
+		}
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
