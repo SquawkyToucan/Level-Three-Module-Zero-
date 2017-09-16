97359bca5f9eded02e851d98e94fc5e59ab38e27
Garrett-Thomas <league@WTS5.attlocal.net>
2017-09-15 18:30:21 -0700

    Started the final recipe in Module Zero
    soon

--- src/IntroToStacks/TextUndoRedo.java
+++ src/IntroToStacks/TextUndoRedo.java
@@ -1,8 +1,17 @@
 package IntroToStacks;
 
-public class TextUndoRedo {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class TextUndoRedo implements KeyListener {
 	/* 
-	 * Create a JFrame with a JPanel and a JLabel.
+	 * Create a JFrame with a JPanel and a JLabel. <COMPLETED>
 	 * 
 	 * Every time a key is pressed, add that character to the JLabel. It should look like a basic text editor.
 	 * 
@@ -13,5 +22,46 @@ public class TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel text = new JLabel();
+	Stack<Character> history = new Stack<Character>(); 
+	public void createGUI() {
+		panel.add(text);
+		frame.addKeyListener(this);
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	public static void main(String[] args) {
+		TextUndoRedo tur = new TextUndoRedo();
+		tur.createGUI();
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getKeyCode() != KeyEvent.VK_BACK_SPACE) {
+			text.setText(text.getText() + e.getKeyChar());
+			frame.pack();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+			char a = text.getText().charAt(text.getText().length()-2);
+			history.add(a);
+			JOptionPane.showMessageDialog(null, history);
+		}
+		else {
+			System.out.println("The logic is wrong");
+		}
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
