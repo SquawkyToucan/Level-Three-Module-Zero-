90883ca59cb42e3e7ca1d135b2e6327073a8d15a
Garrett-Thomas <league@WTS5.attlocal.net>
2017-09-15 17:47:31 -0700

    Finished HashMaps, beginning Stacks

--- src/IntroToHashMaps/LogSearch.java
+++ src/IntroToHashMaps/LogSearch.java
@@ -1,6 +1,43 @@
 package IntroToHashMaps;
 
-public class LogSearch {
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.HashMap;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class LogSearch implements MouseListener {
+	//Hashmap for the program
+	HashMap<Integer, String> names = new HashMap<Integer, String>();
+	//GUI variables
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton addNames = new JButton();
+	JButton search = new JButton();
+	JButton viewList = new JButton();
+	JButton remove = new JButton();
+	LogSearch() {
+		//Set the text of all of the buttons before you use them
+		addNames.setText("Add a name"); addNames.addMouseListener(this);
+		search.setText("Search for a name"); search.addMouseListener(this);
+		viewList.setText("View the names"); viewList.addMouseListener(this);
+		remove.setText("Remove a name"); remove.addMouseListener(this);
+		//Add buttons to panel
+		panel.add(addNames);
+		panel.add(search);
+		panel.add(viewList);
+		panel.add(remove);
+		//Add to frame, visibility, etc.
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	public static void main(String[] args) {
+		new LogSearch();
+	}
   /* 
 	 * Crate a HashMap of Integers for the keys and Strings for the values.
 	 * Create a GUI with three buttons. 
@@ -28,4 +65,57 @@ public class LogSearch {
 	 * 				is not in the list. 
 	 *
 	 * */
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		Object button = e.getSource();
+		if(button == addNames) {
+			String ID = JOptionPane.showInputDialog("Please enter in an Integer ID for the name you're adding.");
+			int id = Integer.parseInt(ID);
+			String name = JOptionPane.showInputDialog("Enter the name of the person being assigned to the ID " + id);
+			names.put(id, name);
+		}
+		else if(button == search) {
+			String idToSearch = JOptionPane.showInputDialog("Enter in the ID you'd like to search for.");
+			int id = Integer.parseInt(idToSearch);
+			if(names.get(id) != null) {
+				JOptionPane.showMessageDialog(null, "Your search returned this result:\n" + names.get(id));
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "Sorry, but this name couldn't be found.");
+			}
+		}
+		else if(button == viewList) {
+			JOptionPane.showMessageDialog(null, names.values());
+		}
+		else if(button == remove) {
+			String removeID = JOptionPane.showInputDialog("What is the ID that you want to remove?");
+			int id = Integer.parseInt(removeID);
+			if(names.get(id) != null) {
+				names.remove(id);
+				JOptionPane.showMessageDialog(null, "This has been removed.");
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "Sorry, this name could not be found.");
+			}
+		}
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/IntroToStacks/IntroToStack.java
+++ src/IntroToStacks/IntroToStack.java
@@ -1,21 +1,41 @@
 package IntroToStacks;
 
+import java.util.Random;
+import java.util.Stack;
+
+import javax.swing.JOptionPane;
+
 public class IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
-		
+		Stack<Double> doubles = new Stack<Double>();
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
+		for(int i = 0; i < 100; i++) {
+			doubles.push(new Random().nextDouble() * 100);
+		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
+		int num1 = Integer.parseInt(JOptionPane.showInputDialog("Please enter in a number between 1 and 100."));
+		int num2 = Integer.parseInt(JOptionPane.showInputDialog("Please enter in a different number between 1 and 100."));
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
-		
+		System.out.println("NUM 1: " + num1 + "\nNUM 2: " + num2);
 		//   EXAMPLE:
 		//   NUM 1: 65
 		//   NUM 2: 75
-		
+		for(int i = 99; i > 0; i--) {
+			if(num2 > num1) {
+				if(doubles.get(i) < num2 && doubles.get(i) > num1) {
+					System.out.println(doubles.elementAt(i));
+				}
+			}
+			else {
+				if(doubles.get(i) < num1 && doubles.get(i) > num2) {
+					System.out.println(doubles.elementAt(i));
+				}
+			}
+			doubles.pop();
+		}
 		//   Popping elements off stack...
 		//   Elements between 65 and 75:
 		//   66.66876846
